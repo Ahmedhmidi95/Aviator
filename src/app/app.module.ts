595fa5b5910853ab828a6f2cbd21bbde91b99657
Ahmed Hmidi <hmidi.ahmed95@gmail.com>
2018-12-23 21:15:09 +0100

    adding bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
